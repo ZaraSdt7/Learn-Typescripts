5ba403a10274cb96bf20bc1873ef185ec2c27373
zahra <zahra.st7373@gmail.com>
2023-10-01 21:39:46 +0330

    "utiilitytype"

---
+++ Learniing/utilitytype45-46.ts
@@ -0,0 +1,35 @@
+type ICourse ={
+    id:number
+    title:string
+    teacher:string
+}
+type edit = Partial<ICourse>
+type requiredcourse = Required<edit>
+type read = Readonly<ICourse>
+class Course{
+    Addcourse(course:ICourse){}
+    Editcourse(course:edit){}
+    Getallcourse():read[]{
+        return []
+    }
+}
+type Title = "nodejs" | "typescript" | "javascript";
+type Courses = Record<Title,ICourse>;
+const Acourse :Courses ={
+nodejs:{
+id:1,
+title:"nodejs",
+teacher:"aaaa"
+},
+typescript:{
+id:2,
+title:"typescript",
+teacher:"bbb"
+},
+javascript:{
+id:3,
+title:"javascript",
+teacher:"ccc"
+}
+}
+console.log(Acourse);
